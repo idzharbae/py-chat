9aaf613d9547bf58213b2f57038d90979dd04f43
idzhar <idzharbae@gmail.com>
2019-06-13 13:14:19 +0700

    Identification page, redirection, connect event

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -6,16 +6,21 @@ app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
 @app.route('/')
+def login():
+	if 'username' in request.cookies:
+		return redirect(url_for('sessions'))
+	return render_template('login.html')
+
+@app.route('/chat')
 def sessions():
+	if 'username' not in request.cookies:
+		return redirect(url_for('login'))
 	return render_template('session.html', user='world')
 
-def messageReceived(methods=['GET','POST']):
-	print('message was recevied.')
-
-@socketio.on('my event')
+@socketio.on('room-1')
 def handleMyCustomEvent(json, methods=['GET','POST']):
-	print('received my event: '+str(json))
-	socketio.emit('my response', json, callback=messageReceived)
+	print('room-1 message: '+str(json))
+	socketio.emit('message-broadcast', json)
 
 if __name__ == '__main__':
 	socketio.run(app, debug=True)
