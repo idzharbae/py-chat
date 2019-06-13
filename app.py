d1abbf7fc9bf6a0f8452689250c33024bf1e5c83
idzhar <idzharbae@gmail.com>
2019-06-13 21:16:29 +0700

    Online users and chat rooms (unfinished)

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
+connectedUsers = []
 
 @app.route('/')
 def login():
@@ -15,11 +16,14 @@ def login():
 def sessions():
 	if 'username' not in request.cookies:
 		return redirect(url_for('login'))
-	return render_template('session.html', user='world')
+	return render_template('dashboard.html', user='world', connectedUsers=connectedUsers)
 
 @socketio.on('room-1')
 def handleMyCustomEvent(json, methods=['GET','POST']):
 	print('room-1 message: '+str(json))
+	if(json['username']):
+		connectedUsers.append(json['username'])
+		print(connectedUsers)
 	socketio.emit('message-broadcast', json)
 
 if __name__ == '__main__':
